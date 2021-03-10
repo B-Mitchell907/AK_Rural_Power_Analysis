9ed2202f2b118efd7a24bb2fae4d5b9e11908515
Braden <72002023+B-Mitchell907@users.noreply.github.com>
2021-03-10 16:44:58 -0700

    init

---
+++ wind_calcs.py
@@ -0,0 +1,79 @@
+import pandas as pd
+
+
+# determining a default capcity factor for a wind turbine for any given city based of wind power class
+#### actual results will vary
+def default_capacity_factor(df) -> float:
+    wind_power_class = df['wpc'].item()
+    wpc_to_cap_factor = {7:32, 6:29, 5:26, 4:23, 3:21, 2:18, 1:15}
+    return wpc_to_cap_factor[wind_power_class]
+
+
+
+# estimating size for a wind turbine installtion based of total amount of power consumed in city/grid
+### using 30% of overall use. value is near median for other state projects.
+def Turbine_size_est(df, est_cap_factor):
+    total_kwh = df['total_kwh_sold'].item()
+    est_size = total_kwh * 0.3 / (8760 * est_cap_factor)
+    return round(est_size, -2)
+
+
+
+# estimating Capital Expenditure for a given city/village project.
+## estimates are dervied from PCE (Power Cost Equalizer) rate.
+## this can represent how costly it is to produce power in given city.
+#### A Correlation between PCE rate and CapEx is used for estimation.   
+def Wind_est_capex_per_kw(df):
+    pce_cost = df['pce_rate'].item()
+    if pce_cost == 0 or pce_cost == 'nan':
+        capex = 4000
+    elif pce_cost >= 0.75:
+        capex = 30000
+    else:
+        capex = 4000 + pce_cost * 34666
+    return round(capex, -2)
+
+
+
+# Adjusting CapEx relativie to size of turbine installation.
+## Coefficents are taking from a Logistic Regression of past projects with respect to CapEx vs Installation size
+def wind_adjusted_capex_per_kw(default_capex, size):
+    coef_dict = {50: 0.082,  100: 0.07634832, 200: 0.05886931, 500: 0.03408415, 1000: -0.02408789, 2000: -0.24361946}
+    if size >= 2000:
+        adjusted_cap = default_capex * (1 + coef_dict[2000])
+    elif size >= 1000:
+        adjusted_cap = default_capex * (1 + coef_dict[1000])
+    elif size >=  500:
+        adjusted_cap = default_capex * (1 + coef_dict[500])
+    elif size >= 200:
+        adjusted_cap = default_capex * (1 + coef_dict[200]) 
+    elif size >= 100:
+        adjusted_cap = default_capex * (1 + coef_dict[100])
+    else:
+        adjusted_cap = default_capex * (1 + coef_dict[50])
+    return round(adjusted_cap, -2)
+
+
+
+# calculating Levelized Cost Of Energy per kiloWatt-hour
+def Wind_LCOE_per_kwh(interest, inflation, N, capacity_factor, turbine_size_kw, capex):
+    #changing turbine size for cities if turnbine_size_kw == 0
+    if turbine_size_kw == 0:
+        turbine_size_kw = 50
+    else:
+        turbine_size_kw = turbine_size_kw    
+    
+    operating_maintence = 0.036
+    r = interest / 100
+    i = inflation / 100
+    cap_fac = capacity_factor / 100
+
+    capital_recovery_factor = r / (1-(1+r)**(-N))
+    net_present_value = (capex * turbine_size_kw) + sum([( (1+i) / (1+r) )**x for x in range(1,N+1)]) * operating_maintence
+    annual_production_kwh = turbine_size_kw * cap_fac * 8760
+    LCOE = (net_present_value * capital_recovery_factor) / annual_production_kwh   
+    return round(LCOE, 3)
+
+
+
+
